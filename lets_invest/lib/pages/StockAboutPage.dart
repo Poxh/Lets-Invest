5ec0c91ce4626dfe822010e43686c2ca6139d67f
PoxhYT <72230632+PoxhYT@users.noreply.github.com>
2022-08-16 00:56:08 +0200

    Improved stock details page design

--- lets_invest/lib/pages/StockAboutPage.dart
+++ lets_invest/lib/pages/StockAboutPage.dart
@@ -31,9 +31,30 @@ class _StockAboutPageState extends State<StockAboutPage> {
 
   @override
   Widget build(BuildContext context) {
+    var tag = WebsocketAPI.latestInstrumentDetail;
+    bool isIntlSymbolNull = tag["intlSymbol"] == null;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
+        title: Padding(
+          padding: EdgeInsets.only(left: 25.w),
+          child: Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right: 10.w),
+                child: BuilderAPI.buildStockPicture(
+                    StockDetail.fromJson(WebsocketAPI.latestStockDetail).isin,
+                    30.w,
+                    30.h),
+              ),
+              BuilderAPI.buildText(
+                  text: isIntlSymbolNull ? "No data" : tag["intlSymbol"],
+                  color: Colors.white,
+                  fontSize: 30.sp,
+                  fontWeight: FontWeight.bold),
+            ],
+          ),
+        ),
         backgroundColor: Color.fromARGB(255, 8, 8, 15),
         elevation: 1,
         actions: [
@@ -59,31 +80,43 @@ class _StockAboutPageState extends State<StockAboutPage> {
         ],
       ),
       backgroundColor: Color.fromARGB(255, 8, 8, 15),
-      body: SingleChildScrollView(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Divider(
-            color: Color.fromARGB(255, 8, 8, 15),
-            thickness: 2.h,
-          ),
-          Container(
-            color: Color.fromARGB(255, 8, 8, 15),
-            child: Column(
-              children: [
-                buildStockTitle(),
-                Padding(
-                  padding: EdgeInsets.only(top: 40.h, bottom: 50.h),
-                  child: SizedBox(
-                    height: 300.h,
-                    child: builderAPI.buildChart(context),
-                  ),
-                ),
-                ChartFilter(onTap: (() {
-                  print("HELLO");
-                })),
-              ],
+      body: Padding(
+        padding: EdgeInsets.only(top: 10.h),
+        child: SingleChildScrollView(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Divider(
+              color: Color.fromARGB(50, 255, 255, 255),
+              thickness: 3.h,
             ),
-          )
-        ]),
+            Padding(
+              padding: EdgeInsets.only(top: 20.h),
+              child: Container(
+                color: Color.fromARGB(255, 8, 8, 15),
+                child: Column(
+                  children: [
+                    buildHeader(
+                        CalculationAPI.hasMadeLost(
+                            WebsocketAPI.getCurrentStockValue(),
+                            WebsocketAPI.getStartStockValue()),
+                        tag,
+                        isIntlSymbolNull),
+                    Padding(
+                      padding: EdgeInsets.only(top: 40.h, bottom: 50.h),
+                      child: SizedBox(
+                        height: 300.h,
+                        child: builderAPI.buildChart(context),
+                      ),
+                    ),
+                    ChartFilter(onTap: (() {
+                      print("HELLO");
+                    })),
+                  ],
+                ),
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
@@ -196,12 +229,11 @@ class _StockAboutPageState extends State<StockAboutPage> {
               ),
             ],
           ),
-          SizedBox(height: 5.h),
+          SizedBox(height: 10.h),
           BuilderAPI.buildText(
-              text:
-                 tag["exchanges"][0]["nameAtExchange"],
+              text: tag["exchanges"][0]["nameAtExchange"],
               color: Colors.white,
-              fontSize: 30.sp,
+              fontSize: 15.sp,
               fontWeight: FontWeight.bold),
           SizedBox(height: 5.h),
           buildProfitLost(CalculationAPI.hasMadeLost(
@@ -271,32 +303,49 @@ class _StockAboutPageState extends State<StockAboutPage> {
   }
 
   Widget buildProfitLost(bool hasMadeLost) {
-    return Row(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Icon(hasMadeLost ? Icons.arrow_downward : Icons.arrow_upward,
-            color: hasMadeLost ? Colors.red : Colors.green, size: 15.sp),
-        BuilderAPI.buildText(
-            text: CalculationAPI.calculateProfitLostInEUR(
-                        WebsocketAPI.getCurrentStockValue(),
-                        WebsocketAPI.getStartStockValue())
-                    .toStringAsFixed(2)
-                    .replaceAll("-", "") +
-                "€ (" +
-                CalculationAPI.calculateProfitLostInPercentage(
-                        WebsocketAPI.getCurrentStockValue(),
-                        WebsocketAPI.getStartStockValue())
-                    .toStringAsFixed(2)
-                    .replaceAll("-", "") +
-                "%)",
-            color: hasMadeLost ? Colors.red : Colors.green,
-            fontSize: 15.sp,
-            fontWeight: FontWeight.bold),
-        SizedBox(width: 5.w),
-        BuilderAPI.buildText(
-            text: "Seit Start",
-            color: Colors.grey,
-            fontSize: 15.sp,
-            fontWeight: FontWeight.bold),
+        Padding(
+          padding: EdgeInsets.only(bottom: 5.h),
+          child: BuilderAPI.buildText(
+              text: CalculationAPI.calculateProfitLostInEUR(
+                          WebsocketAPI.getCurrentStockValue(),
+                          WebsocketAPI.getStartStockValue())
+                      .toStringAsFixed(2)
+                      .replaceAll("-", "") +
+                  "€",
+              color: Colors.white,
+              fontSize: 20.sp,
+              fontWeight: FontWeight.bold),
+        ),
+        Container(
+          width: 100.w,
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 35, 35, 35),
+              borderRadius: BorderRadius.circular(7.sp)),
+          child: Padding(
+            padding:
+                EdgeInsets.only(left: 5.w, right: 5.w, top: 7.h, bottom: 7.h),
+            child: Row(
+              children: [
+                BuilderAPI.buildText(
+                    text: CalculationAPI.calculateProfitLostInPercentage(
+                                WebsocketAPI.getCurrentStockValue(),
+                                WebsocketAPI.getStartStockValue())
+                            .toStringAsFixed(2)
+                            .replaceAll("-", "") +
+                        "%",
+                    color: hasMadeLost ? Colors.red : Colors.green,
+                    fontSize: 15.sp,
+                    fontWeight: FontWeight.bold),
+                Icon(hasMadeLost ? Icons.arrow_downward : Icons.arrow_upward,
+                    color: hasMadeLost ? Colors.red : Colors.green,
+                    size: 15.sp),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
@@ -338,6 +387,139 @@ class _StockAboutPageState extends State<StockAboutPage> {
                 fontWeight: FontWeight.bold),
           )
         ],
+      ),
+    );
+  }
+
+  static Widget buildHeader(hasMadeLost, tag, isIntlSymbolNull) {
+    return Padding(
+      padding: EdgeInsets.only(left: 20.w, right: 10.w),
+      child: InkWell(
+        splashFactory: NoSplash.splashFactory,
+        onTap: (() {}),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 6, 6, 6),
+            borderRadius: BorderRadius.circular(20.sp),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 2.h),
+                        child: Row(
+                          children: [
+                            BuilderAPI.buildStockPicture(
+                                StockDetail.fromJson(
+                                        WebsocketAPI.latestStockDetail)
+                                    .isin,
+                                30.w,
+                                30.h),
+                            SizedBox(width: 10.w),
+                            SizedBox(
+                              width: 100.w,
+                              child: BuilderAPI.buildText(
+                                  text: isIntlSymbolNull
+                                      ? "No data"
+                                      : tag["intlSymbol"],
+                                  color: Colors.white,
+                                  fontSize: 30.sp,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(2.sp),
+                            child: SizedBox(
+                              width: 210.w,
+                              child: BuilderAPI.buildText(
+                                  text: tag["exchanges"][0]["nameAtExchange"],
+                                  color: Colors.white,
+                                  fontSize: 15.sp,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                  padding:
+                      EdgeInsets.only(bottom: 12.h, right: 15.w, top: 10.h),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      BuilderAPI.buildText(
+                          text: CalculationAPI.calculateProfitLostInEUR(
+                                      WebsocketAPI.getCurrentStockValue(),
+                                      WebsocketAPI.getStartStockValue())
+                                  .toStringAsFixed(2)
+                                  .replaceAll("-", "") +
+                              "€",
+                          color: Colors.white,
+                          fontSize: 20.sp,
+                          fontWeight: FontWeight.bold),
+                      SizedBox(height: 3.h),
+                      Row(
+                        children: [
+                          FittedBox(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 35, 35, 35),
+                                  borderRadius: BorderRadius.circular(7.sp)),
+                              child: Padding(
+                                padding: EdgeInsets.only(
+                                    left: 5.w,
+                                    right: 5.w,
+                                    top: 7.h,
+                                    bottom: 7.h),
+                                child: Row(
+                                  children: [
+                                    BuilderAPI.buildText(
+                                        text: CalculationAPI
+                                                    .calculateProfitLostInPercentage(
+                                                        WebsocketAPI
+                                                            .getCurrentStockValue(),
+                                                        WebsocketAPI
+                                                            .getStartStockValue())
+                                                .toStringAsFixed(2)
+                                                .replaceAll("-", "") +
+                                            "%",
+                                        color: hasMadeLost
+                                            ? Colors.red
+                                            : Colors.green,
+                                        fontSize: 15.sp,
+                                        fontWeight: FontWeight.bold),
+                                    Icon(
+                                        hasMadeLost
+                                            ? Icons.arrow_downward
+                                            : Icons.arrow_upward,
+                                        color: hasMadeLost
+                                            ? Colors.red
+                                            : Colors.green,
+                                        size: 15.sp),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  )),
+            ],
+          ),
+        ),
       ),
     );
   }
