a53239324064ad0d9b7c85295fc3dddd31f8b399
Michael Ernst <72230632+PoxhYT@users.noreply.github.com>
2022-10-22 17:48:51 +0200

    feat: :white_check_mark: Implemented API

---
+++ lets_invest_api/API/API/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web
+
+namespace API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
