67ca828e64af3961969f7a005ddbc2acf4866d11
Michael Ernst <72230632+PoxhYT@users.noreply.github.com>
2022-10-22 21:05:48 +0200

    Added GetDataFromMessage

--- lets_invest_api/lets_invest_api/Program.cs
+++ lets_invest_api/lets_invest_api/Program.cs
@@ -1,9 +1,13 @@
 ﻿using WebSocketSharp;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
 
 class Programm
 {
+    static int lastID = 0;
+
     static async Task Main(string[] args)
     {
         WebSocket webSocket = new WebSocket("wss://api.traderepublic.com/");
@@ -26,16 +30,34 @@ class Programm
     {
         Console.WriteLine("Connected to Trade Republic API");
         webSocket.Send("connect 26 {\"locale\":\"en\",\"platformId\":\"webtrading\",\"platformVersion\":\"opera - 91.0.4516\",\"clientId\":\"app.traderepublic.com\",\"clientVersion\":\"1.16.0\"}");
-        SendMessage(webSocket, "sub 29 {\"type\":\"ticker\",\"id\":\"XF000ETH0019.BHS\"}");
+
+        string isin = "XF000ETH0019.BHS";
+        string messageContent = "{\"type\":\"ticker\",\"id\":\"" + isin + "\"}";
+        SendMessage(webSocket, $"sub {getLastID()} {messageContent}");
     }
 
     static void OnMessage(Object sender, MessageEventArgs e)
     {
-        log(e.Data);
+        string jsonString = GetDataFromMessage(e.Data);
+        if (jsonString == null)  return;
+        JObject json = JObject.Parse(jsonString);
+        log("Price: " + json.GetValue("last")["price"].ToString());
     }
 
     static void SendMessage(WebSocket webSocket, string message)
     {
         webSocket.Send(message);
+    }
+
+    static int getLastID()
+    {
+        lastID++;
+        return lastID;
+    }
+
+    private static string GetDataFromMessage(string messageInput)
+    {
+        int index = messageInput.IndexOf('{') - 1;
+        return index < 0 ? null : messageInput[index..];
     }
 }
