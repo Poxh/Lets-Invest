6a2611ffcbb27140a189fc7ecadcebe2797053ee
Michael Ernst <72230632+PoxhYT@users.noreply.github.com>
2022-10-23 20:55:33 +0200

    Added models

---
+++ lets_invest_api/lets_invest_api/Models/Portfolio.cs
@@ -0,0 +1,16 @@
+﻿namespace lets_invest_api.Models
+{
+    public class Portfolio
+    {
+        public int Id { get; set; }
+
+        public bool IsPublic { get; set; }
+
+        public double Value { get; set; }
+
+        public double Cash { get; set; }
+
+        public List<Stock> Stocks { get; set; } = new List<Stock>();
+        public List<Crypto> Cryptos { get; set; } = new List<Crypto>();
+    }
+}
---
+++ lets_invest_api/lets_invest_api/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace lets_invest_api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string ProfilePicture { get; set; }
+
+        public Portfolio Portfolio { get; set; }
+
+
+    }
+}
