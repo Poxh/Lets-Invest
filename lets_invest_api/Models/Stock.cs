3ffe9a9e8470d5e40310b3128454d1c39be3ede6
Michael Ernst <72230632+PoxhYT@users.noreply.github.com>
2022-10-23 15:41:24 +0200

    Added new api project

---
+++ lets_invest_api/Models/Stock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lets_invest_api.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+
+        public string Isin { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public double price { get; set; }
+
+        public double quantity { get; set; }
+
+        public DateTime boughtAt { get; set; }
+    }
+}
---
+++ lets_invest_api/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lets_invest_api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string DisplayName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public int Follower { get; set; }
+
+        public int Activities { get; set; }
+
+        public Portfolio Portfolio { get; set; } = null!;
+    }
+}
