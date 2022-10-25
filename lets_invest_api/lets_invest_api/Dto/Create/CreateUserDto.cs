3f7811a22c3466a462748cf5d35c27d8fc47ee6a
Michael Ernst <72230632+PoxhYT@users.noreply.github.com>
2022-10-25 22:25:00 +0200

    Improved user controller

---
+++ lets_invest_api/lets_invest_api/Dto/Create/CreateUserDto.cs
@@ -0,0 +1,9 @@
+namespace lets_invest_api.Dto
+{
+    public class CreateUserDto
+    {
+        public string DisplayName { get; set; }
+
+        public string ProfilePicture { get; set; }
+    }
+}
---
+++ lets_invest_api/lets_invest_api/Dto/Update/UpdateUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace lets_invest_api.Dto.Update;
+
+public class UpdateUserDto
+{
+    public int Id { get; set; }
+    
+    public string DisplayName { get; set; }
+
+    public string ProfilePicture { get; set; }
+}
